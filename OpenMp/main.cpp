cc7fe7d189551401de7a9afe3fc1f045b2634822
aspope <a02265263@usu.edu>
2023-12-02 20:23:43 -0700

    fix?

--- OpenMp/main.cpp
+++ OpenMp/main.cpp
@@ -12,9 +12,7 @@ parsing a different dataset.
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <chrono>
-
 #include "csv.hpp"
 #include <time.h>
 #include <sys/time.h>
@@ -57,89 +55,151 @@ vector<Point> readcsv(std::string cat1, std::string cat2, std::string cat3) {
   return points;
 }
 
-void kMeansClustering(int epochs, int k, std::string category1,
-                      std::string category2, std::string category3, int thread_count) {
-  double wallStart = get_wall_time();
-  vector<Point> points = readcsv(category1, category2, category3); // read from file
-  double wallEnd = get_wall_time();
-  printf("Program took %f s to load data\n", (wallEnd - wallStart));
 
-  vector<Point> centroids;
-  srand(time(0)); // need to set the random seed
-                  
-  vector<int> nPoints;
-  vector<double> sumX, sumY, sumZ;
-  int clusterId;
-  int epoch;
-  vector<Point>::iterator it;
-  vector<Point>::iterator c;
-  int i;
-  wallStart = get_wall_time();
-  for (i = 0; i < k; ++i) {
-    centroids.push_back(points.at(rand() % points.size()));
-  }
-#pragma omp parallel num_threads(thread_count) \
-        default(none) shared(points, centroids) \
-          private(it, clusterId, c)
+void kMeansClustering_serial(int epochs, int k, vector<Point> &points, vector<Point> &centroids) {
+  
+  double startTime = get_wall_time();
+  for (int e = 0; e < epochs; e++)
   {
+    std::cout << "Serial epoch:" << e << std::endl;
+    for (std::vector<Point>::iterator c = begin(centroids); c != end(centroids); ++c) {
+      // quick hack to get cluster index
+      int clusterId = c - begin(centroids);
 
-  for (c = begin(centroids); c != end(centroids); ++c) {
-    // quick hack to get cluster index
-    clusterId = c - begin(centroids);
-#pragma omp for
-    for (it = points.begin(); it != points.end();
-         ++it) {
+      for (std::vector<Point>::iterator it = points.begin(); it != points.end();
+          ++it) {
 
-      Point p = *it;
-      double dist = c->distance(p);
-      if (dist < p.minDist) {
-        p.minDist = dist;
-        p.cluster = clusterId;
+        Point p = *it;
+        double dist = c->distance(p);
+        if (dist < p.minDist) {
+          p.minDist = dist;
+          p.cluster = clusterId;
+        }
+        *it = p;
       }
-      *it = p;
+    }
+
+    std::vector<int> nPoints;
+    std::vector<double> sumX, sumY, sumZ;
+
+    // Initialize with zeroes
+    for (int j = 0; j < k; ++j) {
+      nPoints.push_back(0);
+      sumX.push_back(0.0);
+      sumY.push_back(0.0);
+      sumZ.push_back(0.0);
+    }
+
+    // Iterate over points to append data to centroids
+    for (std::vector<Point>::iterator it = points.begin(); it != points.end(); ++it) {
+      int clusterId = it->cluster;
+      nPoints[clusterId] += 1;
+      sumX[clusterId] += it->x;
+      sumY[clusterId] += it->y;
+      sumZ[clusterId] += it->z;
+
+      it->minDist = __DBL_MAX__; // reset distance
     }
   }
+  double endTime = get_wall_time();
+  double totalTime = endTime - startTime;
+  double averageTime = totalTime / epochs;
+  printf("Algorithm took %f time to complete and averaged %f per epoch\n", totalTime, averageTime);
 }
 
-  // Initialize with zeroes
-  //
-  for (int j = 0; j < k; ++j) {
-    nPoints.push_back(0);
-    sumX.push_back(0.0);
-    sumY.push_back(0.0);
-    sumZ.push_back(0.0);
+void kMeansClustering(int epochs, int k, vector<Point> &points, vector<Point> &centroids, int thread_count) {
+
+  double startTime = get_wall_time();
+  for (int e = 0; e < epochs; e++)
+  {
+    std::cout << "OpenMp epoch:" << e << std::endl;
+    for (int c = 0; c<centroids.size(); c++) {
+      // quick hack to get cluster index
+      int clusterId = c;
+      Point centroid = centroids[c];
+
+#pragma omp parallel for num_threads(thread_count)
+      for (int i = 0; i<points.size();++i) {
+        Point p = points[i];
+        double dist = centroid.distance(p);
+        if (dist < p.minDist) {
+          p.minDist = dist;
+          p.cluster = clusterId;
+        }
+        points[i] = p;
+      }
   }
 
-      // Iterate over points to append data to centroids
-#pragma omp parallel num_threads(thread_count) \
-        default(none) shared(points,epochs, centroids, sumX, sumY, sumZ, nPoints) \
-          private(it, clusterId, c,epoch)
-          {      
-      for (epoch = 0; epoch < epochs; epoch++){
-        #pragma omp for 
-        for (it = points.begin(); it != points.end(); ++it) {
-          clusterId = it->cluster;
-          nPoints[clusterId] += 1;
-          sumX[clusterId] += it->x;
-          sumY[clusterId] += it->y;
-          sumZ[clusterId] += it->z;
+    std::vector<int> nPoints;
+    std::vector<double> sumX, sumY, sumZ;
 
-          it->minDist = __DBL_MAX__; // reset distance
-        }
+    // Initialize with zeroes
+    for (int j = 0; j < k; ++j) {
+      nPoints.push_back(0);
+      sumX.push_back(0.0);
+      sumY.push_back(0.0);
+      sumZ.push_back(0.0);
+    }
 
-        // Compute the new centroids
-        #pragma omp for
-        for (c = begin(centroids); c != end(centroids); ++c) {
-          clusterId = c - begin(centroids);
-          c->x = sumX[clusterId] / nPoints[clusterId];
-          c->y = sumY[clusterId] / nPoints[clusterId];
-          c->z = sumZ[clusterId] / nPoints[clusterId];
-        }
-      }
-      }
-  wallEnd = get_wall_time();
-  printf("Program took %f s to compute\n", (wallEnd - wallStart));
-    
+    // Iterate over points to append data to centroids
+#pragma omp parallel for num_threads(thread_count) 
+    for (int i = 0; i< points.size(); ++i) {
+      int clusterId = points[i].cluster;
+      nPoints[clusterId] += 1;
+      sumX[clusterId] += points[i].x;
+      sumY[clusterId] += points[i].y;
+      sumZ[clusterId] += points[i].z;
+
+      points[i].minDist = __DBL_MAX__; // reset distance
+    }
+  }
+  double endTime = get_wall_time();
+  double totalTime = endTime - startTime;
+  double averageTime = totalTime / epochs;
+  printf("Algorithm took %f time to complete and averaged %f per epoch\n", totalTime, averageTime);
+}
+
+int main(int argv, char *argc[]) {
+  int thread_count = strtol(argc[1], NULL, 10);
+  int numEpochs = 5;
+  int k = 3;
+  char category1[] = "danceability";
+  char category2[] = "loudness";
+  char category3[] = "instrumentalness";
+
+  double wallStart = get_wall_time();
+  vector<Point> points = readcsv(category1, category2, category3); // read from file
+  vector<Point> centroids;
+  double wallEnd = get_wall_time();
+
+  printf("Program took %f s to load data\n", (wallEnd - wallStart));
+
+  srand(100); // need to set the random seed
+  for (int i = 0; i < k; ++i) {
+    centroids.push_back(points.at(rand() % points.size()));
+  }
+  vector<Point> serial = points; 
+
+
+  kMeansClustering(numEpochs, k, points, centroids, thread_count);
+  kMeansClustering_serial(numEpochs, k, serial, centroids);
+
+  // Validate serial results with cuda
+  int error_count = 0;
+  for (int i = 0; i < points.size(); i++)
+  {
+    if (points[i].cluster != serial[i].cluster)
+    {
+      error_count++;
+    }
+  }
+  if (error_count > 0)
+    printf("%d out of %d point clusters do not match", error_count, points.size());
+  else
+    printf("Parrallel implementation verified with serial");
+
+  printf("\n");
+
   wallStart = get_wall_time();
   ofstream myfile;
   myfile.open("tracks_output.csv");
@@ -152,13 +212,6 @@ void kMeansClustering(int epochs, int k, std::string category1,
   myfile.close();
   wallEnd = get_wall_time();
   printf("Program took %f s to write data\n", (wallEnd - wallStart));
-}
 
-int main(int argv, char *argc[]) {
-  int thread_count = strtol(argc[1], NULL, 10);
-  int numEpochs = 1;
-  int k = 3;
-  kMeansClustering(numEpochs, k, "danceability", "loudness",
-                   "instrumentalness", thread_count);
   return 0;
 }
