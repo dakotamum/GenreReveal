ead96c83571b2c81d5ea7a07453dfd23f9e09e15
Drew Tuttle <dt0700@gmail.com>
2023-12-03 23:06:09 -0700

    Added DistMemGPU functionalitty

---
+++ DistMemGpu/cuda_kMeans.cu
@@ -0,0 +1,138 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "cuda_kMeans.cuh"
+#include "cuda.h"
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+
+/**
+ * Kernel:      kMeansClustering_kernel
+ * 
+ * In Args:     Point* points, Point* centroids, int points_size, int centroids_size, int* nPoints,
+                double* sumX, double* sumY, double* sumZ
+ * Desc:        Carries out the KMeansClustering on points, assigning a cluster to each point
+*/
+__global__
+void kMeansClustering_kernel(Point* points, Point* centroids, int points_size, int centroids_size, int* nPoints,
+                            double* sumX, double* sumY, double* sumZ)
+{
+    int idx = threadIdx.x + blockIdx.x * blockDim.x;
+
+    // Declare shared memory arrays for each block
+    __shared__ int shared_nPoints[3];
+    __shared__ double shared_sumX[3];
+    __shared__ double shared_sumY[3];
+    __shared__ double shared_sumZ[3];
+
+    // Initialize shared memory for each cluster
+    if (threadIdx.x < centroids_size) {
+        shared_nPoints[threadIdx.x] = 0;
+        shared_sumX[threadIdx.x] = 0.0;
+        shared_sumY[threadIdx.x] = 0.0;
+        shared_sumZ[threadIdx.x] = 0.0;
+    }
+
+    __syncthreads();
+
+    if (idx < points_size) {
+        for (int clusterId = 0; clusterId < centroids_size; clusterId++) {
+            // quick hack to get cluster index
+            Point c = centroids[clusterId];
+            Point p = points[idx];
+            // Distance() funtion brought in due to being __host__
+            double dist = (p.x - c.x) * (p.x - c.x) + (p.y - c.y) * (p.y - c.y) +
+                    (p.z - c.z) * (p.z - c.z);
+            if (dist < p.minDist) {
+                p.minDist = dist;
+                p.cluster = clusterId;
+            }
+            points[idx] = p;
+        }
+
+        // Append data to shared memory in the block
+        int clusterId = points[idx].cluster;
+        atomicAdd(&shared_nPoints[clusterId], 1);
+        atomicAdd(&shared_sumX[clusterId], points[idx].x);
+        atomicAdd(&shared_sumY[clusterId], points[idx].y);
+        atomicAdd(&shared_sumZ[clusterId], points[idx].z);
+
+        points[idx].minDist = __DBL_MAX__; // reset distance
+    }
+
+    __syncthreads(); // Synchronize threads within the block before updating global memory
+
+    // Update global memory with results from shared memory
+    if (threadIdx.x < centroids_size) {
+        atomicAdd(&nPoints[threadIdx.x], shared_nPoints[threadIdx.x]);
+        atomicAdd(&sumX[threadIdx.x], shared_sumX[threadIdx.x]);
+        atomicAdd(&sumY[threadIdx.x], shared_sumY[threadIdx.x]);
+        atomicAdd(&sumZ[threadIdx.x], shared_sumZ[threadIdx.x]);
+    }
+}
+
+
+__global__
+void HelloKernel(void)
+{
+    printf("HELLO FROM A CUDA KERNEL\n");
+}
+
+/**
+ * wrapper:     Cuda_KMeans::do_cuda_kMeans
+ * 
+ * In Args:     int k, char* category1,
+                char* category2, char* category3, 
+                Point* points, Point* centroids, int points_size, int centroids_size
+ * Desc:        Setup for device, allocates cuda memory for GPU implementation of KMeansClustering
+*/
+namespace Cuda_KMeans {
+	void do_cuda_kMeans(int k, 
+                      Point* points, Point* centroids, int points_size, int centroids_size, 
+                      int* nPoints,
+                      double* sumX,
+                      double* sumY,
+                      double* sumZ)
+	{
+
+        // Allocate cuda memory for points and centroids
+        Point* cuda_points, *cuda_centroids;
+        cudaMalloc((void**)&cuda_points, points_size * sizeof(Point));
+        cudaMalloc((void**)&cuda_centroids, centroids_size * sizeof(Point));
+        cudaMemcpy(cuda_points, points, points_size * sizeof(Point), cudaMemcpyHostToDevice);
+
+        int* cuda_nPoints;
+        double* cuda_sumX, *cuda_sumY, *cuda_sumZ;
+        cudaMalloc((void**)&cuda_nPoints, k * sizeof(int));
+        cudaMalloc((void**)&cuda_sumX, k * sizeof(double));
+        cudaMalloc((void**)&cuda_sumY, k * sizeof(double));
+        cudaMalloc((void**)&cuda_sumZ, k * sizeof(double));
+
+        cudaMemcpy(cuda_centroids, centroids, centroids_size * sizeof(Point), cudaMemcpyHostToDevice);
+        cudaMemcpy(cuda_nPoints, nPoints, k * sizeof(int), cudaMemcpyHostToDevice);
+        cudaMemcpy(cuda_sumX, sumX, k * sizeof(double), cudaMemcpyHostToDevice);
+        cudaMemcpy(cuda_sumY, sumY, k * sizeof(double), cudaMemcpyHostToDevice);
+        cudaMemcpy(cuda_sumZ, sumZ, k * sizeof(double), cudaMemcpyHostToDevice);
+
+        int blockSize = 256;
+        int numBlocks = (points_size + blockSize - 1) / blockSize;
+
+        kMeansClustering_kernel<<<numBlocks, blockSize>>>(cuda_points, cuda_centroids, points_size, centroids_size, cuda_nPoints, cuda_sumX, cuda_sumY, cuda_sumZ);
+        cudaDeviceSynchronize();
+        
+        // Copy points memory from device to host
+        cudaMemcpy(points, cuda_points, points_size * sizeof(Point), cudaMemcpyDeviceToHost);
+        cudaMemcpy(centroids, cuda_centroids, points_size * sizeof(Point), cudaMemcpyDeviceToHost);
+        cudaMemcpy(nPoints, cuda_nPoints, k * sizeof(int), cudaMemcpyDeviceToHost);
+        cudaMemcpy(sumX,cuda_sumX,  k * sizeof(double), cudaMemcpyDeviceToHost);
+        cudaMemcpy(sumY,cuda_sumY,  k * sizeof(double), cudaMemcpyDeviceToHost);
+        cudaMemcpy(sumZ,cuda_sumZ, k * sizeof(double), cudaMemcpyDeviceToHost);
+        cudaFree(cuda_points);
+        cudaFree(cuda_centroids);
+        cudaFree(cuda_nPoints);
+        cudaFree(cuda_sumX);
+        cudaFree(cuda_sumY);
+        cudaFree(cuda_sumZ); 
+	}
+}
